748846934c65d2566a370b969553d9b5a0899d5c
Xirado <bquickzz0@gmail.com>
2022-08-11 11:17:00 +0200

    Move functions and create JSONObject.get<>()

---
+++ src/main/kotlin/at/xirado/simplejson/array.kt
@@ -0,0 +1,17 @@
+package at.xirado.simplejson
+
+inline fun <reified T> JSONArray.collect() = (0 until length()).map { get<T>(it) }
+
+inline fun <reified T> JSONArray.asSequence() = (0 until length()).asSequence().map { get<T>(it) }
+
+inline fun <reified T> JSONArray.get(index: Int) = when (T::class) {
+    String::class -> getString(index) as T
+    Int::class -> getInt(index) as T
+    Long::class -> getLong(index) as T
+    Double::class -> getDouble(index) as T
+    Float::class -> getDouble(index).toFloat() as T
+    Boolean::class -> getBoolean(index) as T
+    JSONObject::class -> getObject(index) as T
+    JSONArray::class -> getArray(index) as T
+    else -> throw IllegalArgumentException("Cannot get object of type ${T::class.simpleName}")
+}
